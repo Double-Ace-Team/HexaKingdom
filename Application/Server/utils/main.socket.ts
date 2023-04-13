d2cc4cfa426168a368010b90025bf9827d869f00
dimicom3 <dimitrijefilipovic2000@gmail.com>
2023-04-14 00:05:24 +0200

    merge

--- Application/Server/utils/main.socket.ts
+++ Application/Server/utils/main.socket.ts
@@ -2,15 +2,15 @@ import SocketInterface from "./socketInterface";
 import { Socket} from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
-class AppSocket implements SocketInterface
+class MainSocket implements SocketInterface
 {
     handleConnection(socket: Socket): void {
         socket.emit('ping', 'Hi! I am a live socket connection');
     }
-    middlewareImplementation?(soccket: Socket, next) {
+    middlewareImplementation?(soccket: Socket, next: any) {
         return next();
     }
     
 }
 
-export default AppSocket;
+export default MainSocket;
