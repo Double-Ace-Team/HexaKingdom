ebea37e77871022e408905a98b522e5e029f2731
unknown <veljkoturudic@gmail.com>
2023-05-05 00:02:24 +0200

    back-end->makeMove Faulty logic need fix

--- Application/Server/repositories/hexagon.repository.ts
+++ Application/Server/repositories/hexagon.repository.ts
@@ -1,18 +1,23 @@
 import { Hexagon, hexaStatus } from "../Model/Hexagon";
 import { Player } from "../Model/Player";
+import { Army } from "../Model/hexagons/Army";
 import { gamesDB } from "../db/db-model";
 
 export class HexagonRepository
 {
-    async updateSingleHexagon(playerID: string, gameID: string,hexagon: Hexagon, points: number, moves: number)
+    async updateSingleHexagon(playerID: string, gameID: string,hexagon: Army, points: number)
     {
 
         try {
-            const game = await gamesDB.findOneAndUpdate
+            console.log(250, hexagon.moves, hexagon);
+            const game = await gamesDB.updateOne
             (
                 { 
-                    "_id": gameID, "hexagons._id": hexagon._id 
+                    "_id": gameID, "hexagons._id": hexagon._id
                 },  
+                {
+                    "__t": "army"
+                },
                 { 
                     "$set": 
                     {
@@ -22,7 +27,7 @@ export class HexagonRepository
                         "hexagons.$.q": hexagon.q,
                         "hexagons.$.s": hexagon.s,
                         "hexagons.$.r": hexagon.r,
-                        "hexagons.$.moves": moves
+                        "hexagons.$.moves": 50
 
                     }
                 }
--- Application/Server/services/hexagon.service.ts
+++ Application/Server/services/hexagon.service.ts
@@ -6,6 +6,10 @@ import { gamesDB, plainsDB } from "../db/db-model";
 import { Game } from "../Model/Game";
 import {Plain } from "../Model/hexagons/Plain";
 import { Army } from "../Model/hexagons/Army";
+import { set } from "mongoose";
+import { armySchema } from "../db/schema/Hexagons/ArmySchema";
+import { hexaSchema } from "../db/schema/HexagonSchema";
+
 export class HexagonService extends BaseService
 {
     // hs: HexagonService;
@@ -48,15 +52,37 @@ export class HexagonService extends BaseService
                 hexaSrc.r = hexaDst.r;
 
                 let hr = new HexagonRepository();
-                //Kada je f-ja async, OBAVEZNO AWAIT!
-                await hr.updateSingleHexagon("null", gameID, hexaSrc, 0, hexaSrc.moves);
-
                 let game = await gamesDB.findById(gameID);
+                //Kada je f-ja async, OBAVEZNO AWAIT!
+               // hexaSrc.moves = hexaSrc.moves - 1;
+               console.log(hexaSrc);
+                await hr.updateSingleHexagon("null", gameID, hexaSrc, 0);
+                let hexas = game!.hexagons as unknown as Army[];
+                hexas.forEach(h => {
+                    if(h._id == hexaSrc._id)
+                    {
+                        h.q = hexaDst.q;
+                        h.r = hexaDst.r;
+                        h.s = hexaDst.s;
+                        h.moves -= 1;
+                    }
+                })
+                 game!.hexagons = hexas;
+                let indexArmy = game?.hexagons.findIndex(h => h.id === hexaSrc._id) as number;
+                let army = game?.hexagons.filter(h => h.id == hexaSrc._id) as unknown as Army;
+                console.log(army);
+                army.q = hexaDst.q;
+                army.r = hexaDst.r;
+                army.s = hexaDst.s;
+                army.moves -= 1;
+
+                game!.hexagons[indexArmy] = army;
+                
+               
                 game?.hexagons.remove({_id: hexaDst._id});
                 //game?.hexagons = game?.hexagons.filter(h => h._id != hexaDst._id);
-
                 let newPlain: any = new plainsDB({hexaStatus: 0, ownerID:"", points: 0, q: q, r: r, s: s});
-                game?.hexagons.push(newPlain);
+               
 
                 await game?.save();
 
